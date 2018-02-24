bc315e71564da53829e9cf989499c8be13965fba
yevhendevbox <y.d.devbox@gmail.com>
2018-02-24 18:58:38 +0200

    fixed responsive styles, added some js script

--- js/init.js
+++ js/init.js
@@ -133,3 +133,37 @@ $(document).ready(function(){
       });
 
   });
+
+  // Active menu item on scroll
+
+  var menu_selector = ".main-nav"; // Переменная должна содержать название класса или идентификатора, обертки нашего меню. 
+  function onScroll(){
+    var scroll_top = $(document).scrollTop();
+    $(menu_selector + " a").each(function(){
+      var hash = $(this).attr("href");
+      var target = $(hash);
+      if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+        $(menu_selector + " a.active").removeClass("active");
+        $(this).addClass("active");
+      } else {
+        $(this).removeClass("active");
+      }
+    });
+  }
+  $(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    $("a[href^=#]").click(function(e){
+      e.preventDefault();
+      $(document).off("scroll");
+      $(menu_selector + " a.active").removeClass("active");
+      $(this).addClass("active");
+      var hash = $(this).attr("href");
+      var target = $(hash);
+      $("html, body").animate({
+          scrollTop: target.offset().top
+      }, 50, function(){
+        window.location.hash = hash;
+        $(document).on("scroll", onScroll);
+      });
+    });
+  });
